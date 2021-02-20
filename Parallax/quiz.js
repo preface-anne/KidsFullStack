36e1ab7816687bcd730d1e6cd5f96a1ae326cfca
preface-anne <78246409+preface-anne@users.noreply.github.com>
2021-02-20 16:46:44 +0800

    Add files via upload

--- Parallax/quiz.js
+++ Parallax/quiz.js
@@ -1,7 +1,7 @@
 window.addEventListener("scroll", function() {
   const distance = window.scrollY;
   document.querySelector("#background").style.transform = `translateY(${distance * 1.5}px)`;
-  document.querySelector("#cloud1").style.transform = `translateY(${distance * 0.15}px)`;
+  document.querySelector("#cloud1").style.transform = `translateY(${distance * 0.2}px)`;
   document.querySelector("#cloud2").style.transform = `translateY(${distance * 0.2}px)`;
   document.querySelector("#para1").style.transform = `translateY(${distance * 0.1}px)`;
   setTimeout(() => {
